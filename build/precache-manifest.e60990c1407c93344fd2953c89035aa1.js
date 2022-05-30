c35d75569eb5ea72405fa62737442ae73388a8ce
Aaron Weaver <95551676+Atweaver7@users.noreply.github.com>
2022-05-30 11:50:11 -0700

    1

---
+++ build/precache-manifest.e60990c1407c93344fd2953c89035aa1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "e50436e76a1a2c5841fb",
+    "url": "/react-portfolio/static/css/main.6d7e567c.chunk.css"
+  },
+  {
+    "revision": "e50436e76a1a2c5841fb",
+    "url": "/react-portfolio/static/js/main.e50436e7.chunk.js"
+  },
+  {
+    "revision": "9f6f6cec55adb720d38c",
+    "url": "/react-portfolio/static/js/1.9f6f6cec.chunk.js"
+  },
+  {
+    "revision": "d21f458009d5e429d140",
+    "url": "/react-portfolio/static/css/2.11b86d7a.chunk.css"
+  },
+  {
+    "revision": "d21f458009d5e429d140",
+    "url": "/react-portfolio/static/js/2.d21f4580.chunk.js"
+  },
+  {
+    "revision": "e5654c5ed1c4a11b2559",
+    "url": "/react-portfolio/static/js/runtime~main.e5654c5e.js"
+  },
+  {
+    "revision": "91a3a61c6bd91372281d24e6a7c51be0",
+    "url": "/react-portfolio/static/media/BartenderBlitz.91a3a61c.png"
+  },
+  {
+    "revision": "1f31222fa8ef985e6154ee7eeec88fcc",
+    "url": "/react-portfolio/static/media/cover-photo.1f31222f.jpg"
+  },
+  {
+    "revision": "da58a8dcad7fd80f5402d9e51d2be0e8",
+    "url": "/react-portfolio/static/media/Budget-Tracker.da58a8dc.jpeg"
+  },
+  {
+    "revision": "c417d947f82558c1a28b33fff9afce3c",
+    "url": "/react-portfolio/static/media/Note-Taker.c417d947.gif"
+  },
+  {
+    "revision": "148a7c0428800fe1b94f74b864c1262b",
+    "url": "/react-portfolio/static/media/Employee-Tracker.148a7c04.gif"
+  },
+  {
+    "revision": "b4cb9cbef3e8c65ddc6f01a3fc15b884",
+    "url": "/react-portfolio/static/media/TFT-Compare.b4cb9cbe.png"
+  },
+  {
+    "revision": "432df7243ad7adec432e18f97acbb7a4",
+    "url": "/react-portfolio/static/media/Quiz.432df724.png"
+  },
+  {
+    "revision": "f9e5dde7a9918376703488bddc0ef9f9",
+    "url": "/react-portfolio/static/media/Git.f9e5dde7.png"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-portfolio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-portfolio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-portfolio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-portfolio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-portfolio/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "016e8d73ec9d385307c17016e61deac0",
+    "url": "/react-portfolio/index.html"
+  }
+];
